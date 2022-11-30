0579b16110a75e04879c6fb33ac0cd7dd8c0b11b
Kayla Firestack <kfirestack@mbta.com>
2022-11-30 09:57:18 -0500

    chore: convert to tabs

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
-  description = "Flake utils demo";
+	description = "Flake utils demo";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+	inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      {
-        packages = rec {
-          hello = pkgs.hello;
-          default = hello;
-        };
-        apps = rec {
-          hello = flake-utils.lib.mkApp { drv = self.packages.${system}.hello; };
-          default = hello;
-        };
-      }
-    );
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = nixpkgs.legacyPackages.${system}; in
+			{
+				packages = rec {
+					hello = pkgs.hello;
+					default = hello;
+				};
+				
+				apps = rec {
+					hello = flake-utils.lib.mkApp { drv = self.packages.${system}.hello; };
+					default = hello;
+				};
+			}
+		);
 }
