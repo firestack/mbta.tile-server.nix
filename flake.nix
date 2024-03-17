3227ca4c48fcd6a916ab5e32637c256643cdf620
Kayla Firestack <kfirestack@mbta.com>
2024-03-17 16:51:52 -0400

    fix: make inputs follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   description = "virtual environments";
 
   inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "/nixpkgs";
+
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.flake-parts.inputs.nixpkgs-lib.follows = "/nixpkgs";
 
   outputs = inputs@{ self, flake-parts, devshell, nixpkgs }:
     flake-parts.lib.mkFlake { inherit inputs; } {
