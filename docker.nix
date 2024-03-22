8ef4fa6826d32227965972c1d1cc2ea6ac7e8d14
Kayla Firestack <kfirestack@mbta.com>
2024-03-22 08:58:07 -0400

    wip! docker image

--- docker.nix
+++ docker.nix
@@ -1,38 +1,43 @@
 { lib
-, stdenvNoCC
+, dockerTools
 , postgresql
 , osm2pgsql
 , mapnik
 , carto
+, bash
+}: dockerTools.buildLayeredImage {
+	name = "MBTA Tile Server";
+	tag = "nix";
 
-{
-					buildinputs = with pkgs; [
-						(postgresql.withPackages (ps: [
-							ps.postgis
-						]))
+	contents = [
+		(postgresql.withPackages (ps: [
+			ps.postgis
+		]))
 
-						osm2pgsql
+		osm2pgsql
 
-						mapnik
+		mapnik
 
-						carto
+		carto
 
-						## fonts
-						# noto-cjk
-						# noto-hinted
-						# noto-unhinted
-						# hanazono
-						# unifont
-						# dejavu
-						# dejavu-core
-						# dejavu-extra
-						# cabextrat
+		bash
 
-						# todo: figure out how to render with and without apache
-						# apache
-						# apacheHttpdPackages.mod_tile
-						# # renderd # provided by `mod_tile`
-						
-						kosmtik
-					];
-				};
+		## fonts
+		# noto-cjk
+		# noto-hinted
+		# noto-unhinted
+		# hanazono
+		# unifont
+		# dejavu
+		# dejavu-core
+		# dejavu-extra
+		# cabextrat
+
+		# todo: figure out how to render with and without apache
+		# apache
+		# apacheHttpdPackages.mod_tile
+		# # renderd # provided by `mod_tile`
+		
+		# kosmtik
+	];
+}
